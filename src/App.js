fdb5c1893024390c3462f07a8a5000731aabaaf9
Mikhail Vasin <michaelvasin@gmail.com>
2018-08-16 12:32:45 +0300

    Add prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import {Transition} from 'react-transition-group'
-import { TimelineLite } from "gsap/TweenMax";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Transition } from 'react-transition-group';
+import { TimelineLite } from 'gsap/TweenMax';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,12 +13,20 @@ class App extends Component {
     tl.eventCallback('onComplete', done);
     switch (status) {
       case 'entering':
-        tl.from('.App-header', 3, {opacity: 0})
-          .from('.App-body', 2, {x: '-100%'}, '-=2');
+        tl.from('.App-header', 3, { opacity: 0 }).from(
+          '.App-body',
+          2,
+          { x: '-100%' },
+          '-=2'
+        );
         break;
       case 'exiting':
-        tl.to('.App-header', 2, {opacity: 0}, '+=1')
-          .to('.App-body', 3, {x: '100%'}, '-=3');
+        tl.to('.App-header', 2, { opacity: 0 }, '+=1').to(
+          '.App-body',
+          3,
+          { x: '100%' },
+          '-=3'
+        );
     }
   }
 
@@ -28,10 +36,10 @@ class App extends Component {
     tl.eventCallback('onComplete', done);
     switch (status) {
       case 'entering':
-        tl.from(node, 3, {opacity: 0})
+        tl.from(node, 3, { opacity: 0 });
         break;
       case 'exiting':
-        tl.to(node, 3, {opacity: 0})
+        tl.to(node, 3, { opacity: 0 });
     }
   }
 
@@ -39,26 +47,37 @@ class App extends Component {
     return (
       <Router>
         <React.Fragment>
-          <Route exact path='/'>
-            {({match}) => (
-              <Transition appear in={!!match} unmountOnExit addEndListener={App.homeTransition}>
+          <Route exact path="/">
+            {({ match }) => (
+              <Transition
+                appear
+                in={!!match}
+                unmountOnExit
+                addEndListener={App.homeTransition}
+              >
                 <div className="App">
                   <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                   </header>
                   <p className="App-body">
-                    To get started, edit <code>src/App.js</code> and save to reload. <Link to='/another'>Go another route</Link>
+                    To get started, edit <code>src/App.js</code> and save to
+                    reload. <Link to="/another">Go another route</Link>
                   </p>
                 </div>
               </Transition>
             )}
           </Route>
-          <Route path='/another'>
-            {({match}) => (
-              <Transition appear in={!!match} unmountOnExit addEndListener={App.anotherTransition}>
+          <Route path="/another">
+            {({ match }) => (
+              <Transition
+                appear
+                in={!!match}
+                unmountOnExit
+                addEndListener={App.anotherTransition}
+              >
                 <p>
-                  Hi from another route. <Link to='/'>Go back</Link>
+                  Hi from another route. <Link to="/">Go back</Link>
                 </p>
               </Transition>
             )}
